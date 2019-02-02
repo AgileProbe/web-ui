49cde03d1f0cce4c77dd7deea7894ecaa2b9a6a5
刘子寒 <liuzihan@liuzihandeMBP.lan>
2019-02-02 13:44:06 +0800

    footerText修改

--- src/component/mixin/Config.jsx
+++ src/component/mixin/Config.jsx
@@ -2,7 +2,7 @@ const Main = {
 	target: process.env.NODE_ENV !== 'production' ? 'http://localhost:8080/' : 'http://120.24.238.195:8080/WIFIProbeAnalysis_web-1.0-SNAPSHOT/', //目标网站
     name: 'WIFI Probe Admin',
     prefix: 'SCUNET Admin',
-    footerText: '川行 版权所有 © 2017 All Rights Reserved',
+    footerText: '',
     logoSrc: 'https://t.alipayobjects.com/images/rmsweb/T1B9hfXcdvXXXXXXXX.svg',
     logoText: 'SCUNET Admin',
     needLogin: true,
